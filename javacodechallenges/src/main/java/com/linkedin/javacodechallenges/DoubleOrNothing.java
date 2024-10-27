6c171e81fa10b5a6f67a1c8ea1627f0377ffbc7e
Sergio88Git <98197331+Sergio88Git@users.noreply.github.com>
2024-10-27 22:23:58 +0000

    sync

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,16 +1,36 @@
+package com.linkedin.javacodechallenges;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
   public void playGame() {
     // TODO: Implement method
     int points = 10;
     char answer = ' ';
-    while (points > 0 && answer !='q') {
+    Scanner input = new Scanner(System.in);
+    while (points > 0 && answer != 'q') {
 
-      System.out.println("You have "+points+" points.");
-      System.out.println("(D)ouble or (q)uit ?");
+      System.out.println("You have " + points + " points.");
       do {
-        input answer;
-      } while (answer != 'd' && answer != 'q')
+        System.out.println("(d)ouble or (q)uit ?");
+        answer = input.next().charAt(0);
+      } while (answer != 'd' && answer != 'q');
+      if (answer == 'd') {
+        if (Math.random() >= 0.5) {
+
+          System.out.println("You won !");
+          points = points * 2;
+        } else {
+          System.out.println("you lose !");
+          points = 0;
+        }
+
+      } else {
+        System.out.println("Bye !");
+      }
+
     }
+    System.out.println("You have " + points + " points.");
+
   }
 }
