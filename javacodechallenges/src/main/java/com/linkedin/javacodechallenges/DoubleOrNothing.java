8e371f9618543a640a52c0f99e14700988765deb
Sergio88Git <98197331+Sergio88Git@users.noreply.github.com>
2024-10-27 21:20:36 +0000

    sync

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -2,5 +2,15 @@ public class DoubleOrNothing {
 
   public void playGame() {
     // TODO: Implement method
+    int points = 10;
+    char answer = ' ';
+    while (points > 0 && answer !='q') {
+
+      System.out.println("You have "+points+" points.");
+      System.out.println("(D)ouble or (q)uit ?");
+      do {
+        input answer;
+      } while (answer != 'd' && answer != 'q')
+    }
   }
 }
