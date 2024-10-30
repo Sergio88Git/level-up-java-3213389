6f602b4e3924c38b5f3c5d996715949267f7aee8
Sergio88Git <98197331+Sergio88Git@users.noreply.github.com>
2024-10-30 00:29:28 +0000

    yes

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,11 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+    public static void main(String[] args) {
+        Person onePerson = new Person();
+
+        onePerson.setFirstName("serge");
+        onePerson.setLastName("Beauregard");
+        System.out.println(onePerson.message());
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,29 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+
+  private String firstName;
+  private String lastName;
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public String message() {
+    return ("My name is " + getFirstName() + " " + getLastName() +
+        " and I'm an object.");
+
+  }
 }
